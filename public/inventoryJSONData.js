e651e12b726a018d3e25ee667ae876a10059bcc9
Kwadwo <addoasa94@gmail.com>
2019-11-08 22:25:29 -0500

    Inventory data rendered

--- public/inventoryJSONData.js
+++ public/inventoryJSONData.js
@@ -1,4 +1,4 @@
-const inventory = [
+const inventoryJSON = [
 	{
 		"id": "A",
 		"description": "Apple",
--- public/script/index.js
+++ public/script/index.js
@@ -1,1 +1,26 @@
-console.log(inventory);
+console.log(inventoryJSON);
+
+const inventorySet = document.querySelector('.inventory-set');
+inventorySet.classList.add('inventory-set');
+
+for(let i = 0; i < inventoryJSON.length; i++){
+  let inventoryItem = document.createElement("div");
+  inventoryItem.id = inventoryJSON[i].id;
+  inventoryItem.classList.add('inventory-item')
+  let inventoryItemDesc = document.createElement("h3");
+  inventoryItemDesc.classList.add("inventory-item-desc");
+  inventoryItemDesc.innerText = inventoryJSON[i].description;
+  let inventoryItemPrice = document.createElement("h4");
+  inventoryItemPrice.classList.add("inventory-item-price");
+  inventoryItemPrice.innerText = "$" + inventoryJSON[i].unit_price;
+  // let inventoryItemDiscount = document.createElement("h4");
+  let addToCartBtn = document.createElement('button');
+  addToCartBtn.id = "button"+ inventoryJSON[i].id;
+  addToCartBtn.innerText = "Add to Cart";
+  // inventoryItemDiscount.innerText = "Save:" + " " + "Buy" + inventoryJSON[i].volume_discounts[0].number + " " + "for" + " " + inventoryJSON[i].volume_discounts[0].price;
+  inventoryItem.appendChild(inventoryItemDesc);
+  inventoryItem.appendChild(inventoryItemPrice);
+  // inventoryItem.appendChild(inventoryItemDiscount);
+  inventoryItem.appendChild(addToCartBtn);
+  inventorySet.appendChild(inventoryItem);
+}
